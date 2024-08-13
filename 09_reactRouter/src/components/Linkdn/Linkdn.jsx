3a6195b78b2a6a2ebc46134cb4b2ddb5cf8dfd78
singhshadow <singhshadow008@gmail.com>
2024-08-13 10:34:40 +0000

    09_reactRouter practical Linkdn work

--- 09_reactRouter/src/components/Linkdn/Linkdn.jsx
+++ 09_reactRouter/src/components/Linkdn/Linkdn.jsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react'
 function Linkdn() {
     const [data, setData] =useState([])
     useEffect(() => {
-        fetch('https://api.linkdn.com/users/akshatjain')
+        // fetch('https://api.github.com/users/hiteshchoudhary')
+        fetch('https://api.github.com/users/singhshadow')
         .then(response => response.json())
         .then(data => {
             console.log(data)
@@ -11,7 +12,9 @@ function Linkdn() {
         })
     }, [])
   return (
-    <div className='bg-gray-700 text-center p-5 text-4xl text-red-700 font-semibold'>LinkDn Followers: </div>
+    <div className='bg-gray-700 text-center p-5 text-4xl text-red-700 font-semibold'>LinkDn Followers: {data.followers}
+    <img src={data.avatar_url} alt="not found" width={300} />
+    </div>
   )
 }
 
