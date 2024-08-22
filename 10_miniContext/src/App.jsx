0d86aaf885abe4fbff3b49c4da5f5ecdcf063631
singhshadow <singhshadow008@gmail.com>
2024-08-22 10:57:24 +0000

    10_miniContext practicals

--- 10_miniContext/src/App.jsx
+++ 10_miniContext/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import UserContextProvider from './context/UserContextProvider'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
       <h1>react with chai and follow to @arey_aks_07 on instagram</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
